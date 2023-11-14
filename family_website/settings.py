60a8a4d16e362f078f0e400494a800f7da4cf854
CarstenS <starke.carsten@gmx.de>
2023-11-14 20:29:40 +0100

    replaced pointer for base.html

--- family_website/settings.py
+++ family_website/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'family_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['family_website/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'family_website/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
