67d5b19b9605064ee7ba3476aeb1a15f06899052
CarstenS <starke.carsten@gmx.de>
2023-11-17 21:27:01 +0100

    added signup in views and html

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        # user has info and wants an account now, i.e. post request
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user=user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords must match!'})
+    else:
+        # user wants to enter info, i.e. get request
+        return render(request, 'accounts/signup.html')
+
 
 def login(request):
     return render(request, 'accounts/login.html')
+
 
 def logout(request):
     # TODO route the user back to the homepage
