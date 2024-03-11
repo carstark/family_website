b9f53940dc640efbee8da1688466685b50f44dbb
CarstenS <starke.carsten@gmx.de>
2024-03-11 16:27:17 +0100

    sending key via email to reset passwords

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from blog.models import Blog, Vote
-
-
-given_username = "Mr.X"
+import smtplib
+from email.message import EmailMessage
+import random
+import string
+from datetime import datetime
 
 
 def signup(request):
@@ -46,8 +48,9 @@ def forgotten(request):
     if request.method == 'POST':
         if request.POST['username'] != "":
             try:
-                user = User.objects.get(username=request.POST['username'])
                 given_username = request.POST['username']
+                user = User.objects.get(username=given_username)
+                send_key(given_username, user.email, request)
                 return render(request, 'accounts/reset.html', {'given_username': given_username})
             except User.DoesNotExist:
                 return render(request, 'accounts/forgotten.html', {'error': 'Username does not exist!'})
@@ -58,23 +61,52 @@ def forgotten(request):
         return render(request, 'accounts/forgotten.html')
 
 
+def send_key(name, email, request):
+    global sent_key, sent_time
+    try:
+        s = smtplib.SMTP(host=host)
+        s.starttls()
+        s.login(uname, pword)
+    except Exception as e:
+        print(f"Internet problem --> {e}")
+        return render(request, 'accounts/forgotten.html', {'error': f'{e} -> Internet problem!?'})
+
+    try:
+        msg = EmailMessage()
+        msg["From"] = uname
+        msg["Subject"] = f"{name}'s One-Time-Key is..."
+        msg["Bcc"] = email
+        sent_key = ''.join(random.choices(string.digits, k=4))
+        msg.set_content(sent_key)
+        s.send_message(msg)
+    except Exception as e:
+        print(f"Sending problem --> {e}")
+        return render(request, 'accounts/forgotten.html', {'error': f'{e} -> Sending problem!?'})
+
+    sent_time = datetime.now()
+    s.quit()
+
+
 def reset(request):
-    global given_username
+    global given_username, sent_key, sent_time
     if request.method == 'POST':
         # user has info and wants an account now, i.e. post request
-        if request.POST['password1'] == request.POST['password2']:
-            if request.POST['key'] == "007":
-                user = User.objects.get(username=given_username)
-                user.set_password(request.POST['password1'])
-                user.save()
-                auth.login(request, user=user)
-                return redirect('home')
+        if (datetime.now() - sent_time).total_seconds() < 100:
+            if request.POST['password1'] == request.POST['password2']:
+                if request.POST['key'] == sent_key:
+                    user = User.objects.get(username=given_username)
+                    user.set_password(request.POST['password1'])
+                    user.save()
+                    auth.login(request, user=user)
+                    return redirect('home')
+                else:
+                    return render(request, 'accounts/reset.html', {'error': 'Key is wrong!',
+                                                                   'given_username': given_username})
             else:
-                return render(request, 'accounts/reset.html', {'error': 'Key is wrong!',
+                return render(request, 'accounts/reset.html', {'error': 'Passwords must match!',
                                                                'given_username': given_username})
         else:
-            return render(request, 'accounts/reset.html', {'error': 'Passwords must match!',
-                                                           'given_username': given_username})
+            return render(request, 'accounts/forgotten.html', {'error': 'Key timed out!'})
     else:
         # user wants to enter info, i.e. get request
         return render(request, 'accounts/reset.html')
